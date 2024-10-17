9bd97a79a18fb4d28ae2009b8aeeb06e09689ccc
levisstrauss <levisstrauss11@yahoo.com>
2024-10-17 11:10:33 -0400

    Initial, & component initialization

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {Title} from "./components/Title.jsx";
+
+function App() {
+  return (
+    <main>
+      <div>
+        <Title />
+        <p>0</p>
+        <button>Reset</button>
+        <div>
+          <button>-</button>
+          <button>+</button>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Title.jsx
@@ -0,0 +1,6 @@
+
+export const Title = () => {
+    return (
+        <h1>Fancy Counter</h1>
+    )
+}
